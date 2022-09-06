90b7bd78e699477ce925aaba0d6cc3c1e0426c66
João Victor <jjoaovvictorneves@gmail.com>
2022-09-05 23:27:27 -0300

    Update MainActivity with main navigation

--- app/src/main/java/br/com/suacifra/MainActivity.kt
+++ app/src/main/java/br/com/suacifra/MainActivity.kt
@@ -1,11 +1,40 @@
 package br.com.suacifra
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import br.com.suacifra.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationMenuView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replaceFragment(Home())
+
+
+        binding.mainBottomNavigationView.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.homeBottomNavigation -> {
+                    replaceFragment(Home())
+                }
+                R.id.addBottomNavigation -> replaceFragment(Add())
+                R.id.settingsBottomNavigation -> replaceFragment(Settings())
+                else -> {}
+            }
+            true
+        }
     }
+
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.mainFrameLayout, fragment)
+        fragmentTransaction.commit()
+    }
+
 }
