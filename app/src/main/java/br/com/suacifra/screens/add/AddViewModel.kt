04d27d316bb1e880f34b0d0c6e94c0dd7883496a
João Victor <jjoaovvictorneves@gmail.com>
2022-09-10 20:28:39 -0300

    Create AddViewModel.kt

---
+++ app/src/main/java/br/com/suacifra/screens/add/AddViewModel.kt
@@ -0,0 +1,13 @@
+package br.com.suacifra.screens.add
+
+import androidx.lifecycle.ViewModel
+
+class AddViewModel : ViewModel() {
+    init {
+
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+    }
+}
