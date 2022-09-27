a84485111d877e56291c7aac317a299bf5233a25
João Victor <jjoaovvictorneves@gmail.com>
2022-09-27 10:55:24 -0300

    Add Login first interaction and screen

---
+++ app/src/main/java/br/com/suacifra/screens/login/LoginFragment.kt
@@ -0,0 +1,30 @@
+package br.com.suacifra.screens.login
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import br.com.suacifra.MainActivity
+import br.com.suacifra.R
+import br.com.suacifra.databinding.LoginFragmentBinding
+
+class LoginFragment : Fragment() {
+
+    private lateinit var binding: LoginFragmentBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        binding = DataBindingUtil.inflate(inflater, R.layout.login_fragment, container, false)
+        binding.signInWithGoogleButton.setOnClickListener {
+//            signInGoogle()
+        }
+
+        return binding.root
+    }
+
+}
---
+++ app/src/main/java/br/com/suacifra/screens/login/LoginViewModel.kt
@@ -0,0 +1,13 @@
+package br.com.suacifra.screens.login
+
+import androidx.lifecycle.ViewModel
+
+class LoginViewModel : ViewModel() {
+    init {
+
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+    }
+}
