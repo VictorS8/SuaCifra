1149ee270f9956e0b54ce238cbb074f62203ff38
João Victor <jjoaovvictorneves@gmail.com>
2022-09-28 21:14:13 -0300

    Update Login Screen with go back button

--- app/src/main/java/br/com/suacifra/screens/login/LoginFragment.kt
+++ app/src/main/java/br/com/suacifra/screens/login/LoginFragment.kt
@@ -6,13 +6,15 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import br.com.suacifra.MainActivity
 import br.com.suacifra.R
 import br.com.suacifra.databinding.LoginFragmentBinding
+import br.com.suacifra.databinding.MainActivityBinding
+import br.com.suacifra.screens.settings.SettingsFragment
 
 class LoginFragment : Fragment() {
 
     private lateinit var binding: LoginFragmentBinding
+    private lateinit var mainActivityBinding: MainActivityBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -20,11 +22,29 @@ class LoginFragment : Fragment() {
     ): View {
         // Inflate the layout for this fragment
         binding = DataBindingUtil.inflate(inflater, R.layout.login_fragment, container, false)
+        mainActivityBinding =
+            DataBindingUtil.inflate(inflater, R.layout.main_activity, container, false)
+
         binding.signInWithGoogleButton.setOnClickListener {
 //            signInGoogle()
         }
 
+        binding.loginGoBackButton.setOnClickListener {
+            goBackToSettings()
+        }
+
         return binding.root
+    }
+
+    private fun goBackToSettings() {
+        val fragmentManager = parentFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.setCustomAnimations(
+            R.anim.slide_in,
+            R.anim.slide_out
+        )
+        fragmentTransaction.replace(mainActivityBinding.mainFrameLayout.id, SettingsFragment())
+        fragmentTransaction.commit()
     }
 
 }
