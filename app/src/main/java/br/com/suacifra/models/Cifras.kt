99fcdb4db0d6875eee61b1491d78144bb90aff9d
João Victor <jjoaovvictorneves@gmail.com>
2022-10-14 02:02:53 -0300

    Update Cifras.kt

--- app/src/main/java/br/com/suacifra/models/Cifras.kt
+++ app/src/main/java/br/com/suacifra/models/Cifras.kt
@@ -2,8 +2,8 @@ package br.com.suacifra.models
 
 data class Cifras(
     val id: Long,
-    val name: String,
-    val tone: Tones,
-    val singerName: String,
-    val chordsSequence: MutableList<MutableList<Tones>>
+    var name: String,
+    var tone: String,
+    var singerName: String,
+    var chordsSequence: MutableList<MutableList<Tones>>
 )
