0d8bc3025405123a9424883254d97431462ab792
João Victor <jjoaovvictorneves@gmail.com>
2022-10-14 01:55:40 -0300

    Update Add screen with a static list

--- app/src/main/java/br/com/suacifra/screens/add/AddFragment.kt
+++ app/src/main/java/br/com/suacifra/screens/add/AddFragment.kt
@@ -9,12 +9,24 @@ import android.widget.Toast
 import androidx.core.view.isVisible
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.Adapter
+import androidx.recyclerview.widget.RecyclerView.LayoutManager
+import br.com.suacifra.MainActivity
 import br.com.suacifra.R
 import br.com.suacifra.databinding.AddFragmentBinding
+import br.com.suacifra.models.Chords
+import br.com.suacifra.models.Tones
 
 class AddFragment : Fragment() {
 
     private lateinit var binding: AddFragmentBinding
+    private lateinit var mainActivityContext: MainActivity
+    private lateinit var sequenceChordsList: MutableList<MutableList<String>>
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var sequenceAdapter: Adapter<SequenceChordsRecyclerViewAdapter.ViewHolder>
+    private lateinit var sequenceLayoutManager: LayoutManager
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,8 +35,38 @@ class AddFragment : Fragment() {
 
         binding = DataBindingUtil.inflate(inflater, R.layout.add_fragment, container, false)
 
+        mainActivityContext = (activity as MainActivity)
+
         binding.chooseToneButton.setOnClickListener {
             checkCardViewVisibility()
+        }
+
+        // TODO - Change to update list
+        sequenceChordsList = fillSequenceChordsArray()
+
+        recyclerView = binding.sequenceRecyclerView
+        recyclerView.hasFixedSize()
+
+        sequenceLayoutManager = LinearLayoutManager(mainActivityContext)
+        recyclerView.layoutManager = sequenceLayoutManager
+
+        sequenceAdapter = SequenceChordsRecyclerViewAdapter(sequenceChordsList, mainActivityContext)
+        recyclerView.adapter = sequenceAdapter
+
+        binding.addSequencesImageButton.setOnClickListener {
+            Toast.makeText(
+                mainActivityContext,
+                "Add one sequence and change screen",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+
+        binding.saveCifraButton.setOnClickListener {
+            Toast.makeText(
+                mainActivityContext,
+                getString(R.string.save_cifra_successfully, binding.cifraNameEditText.text),
+                Toast.LENGTH_SHORT
+            ).show()
         }
 
         allToneClickEvent()
@@ -64,6 +106,27 @@ class AddFragment : Fragment() {
             gToneTextView.setOnClickListener { clickableTextAsTextViewEvent(it) }
             abToneTextView.setOnClickListener { clickableTextAsTextViewEvent(it) }
         }
+    }
+
+    private fun fillSequenceChordsArray(): MutableList<MutableList<String>> {
+        val auxList: MutableList<MutableList<String>> = mutableListOf()
+
+        auxList.addAll(
+            mutableListOf(
+                mutableListOf(Tones.A, Tones.E),
+                mutableListOf(Tones.G, Tones.D, Tones.E + Chords.minor, Tones.C),
+                mutableListOf(Tones.E, Tones.B),
+                mutableListOf(Tones.F, Tones.C, Tones.D + Chords.minor, Tones.B, Tones.ASharp),
+                mutableListOf(Tones.A, Tones.E),
+                mutableListOf(Tones.G, Tones.D, Tones.E + Chords.minor, Tones.C),
+                mutableListOf(Tones.E, Tones.B),
+                mutableListOf(Tones.F, Tones.C, Tones.D + Chords.minor, Tones.B, Tones.ASharp),
+                mutableListOf(Tones.C, Tones.G),
+                mutableListOf(Tones.C, Tones.G, Tones.A + Chords.minor, Tones.F)
+            )
+        )
+
+        return auxList
     }
 
 }
