85246a8209ec2fd5c7435361a6bf267673f1b858
João Victor <jjoaovvictorneves@gmail.com>
2022-10-20 02:33:16 -0300

    Add Config file to set constantes to project

---
+++ app/src/main/java/br/com/suacifra/utils/Config.kt
@@ -0,0 +1,10 @@
+package br.com.suacifra.utils
+
+object Config {
+    const val SUA_CIFRA_DATABASE = "suaCifraDatabase.db"
+    const val SUA_CIFRA_DATABASE_VERSION = 2
+
+    const val NOTE_ID_BUNDLE_KEY = "NOTE_ID_KEY"
+    const val NOTE_TITLE_BUNDLE_KEY = "NOTE_TITLE_KEY"
+    const val NOTE_BODY_BUNDLE_KEY = "NOTE_BODY_KEY"
+}
