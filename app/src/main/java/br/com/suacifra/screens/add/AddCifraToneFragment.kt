05746c0f9fb8baa50e5f5c50e10ab86137a83c3d
João Victor <jjoaovvictorneves@gmail.com>
2023-02-11 02:23:31 -0300

    Add add cifra tone first interactions

---
+++ app/src/main/java/br/com/suacifra/screens/add/AddCifraToneFragment.kt
@@ -0,0 +1,84 @@
+package br.com.suacifra.screens.add
+
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import br.com.suacifra.MainActivity
+import br.com.suacifra.R
+import br.com.suacifra.databinding.AddCifraToneFragmentBinding
+import br.com.suacifra.utils.Config
+import br.com.suacifra.utils.getColorFromAttr
+
+class AddCifraToneFragment : Fragment() {
+
+    private lateinit var binding: AddCifraToneFragmentBinding
+    private lateinit var mainActivityContext: MainActivity
+    private var cifraTone: String = ""
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.add_cifra_tone_fragment,
+            container,
+            false
+        )
+
+        mainActivityContext = (activity as MainActivity)
+
+        binding.addCifraToneStringTextView.text = getString(R.string.add_cifra_tone_hint_text)
+
+        val sharedPref = mainActivityContext.getSharedPreferences(
+            Config.SHARED_PREFERENCE_FILE_KEY, Context.MODE_PRIVATE
+        )
+
+        cifraTone = sharedPref.getString(
+            Config.SHARED_PREFERENCE_EDIT_CIFRA_MODE_TONE_STRING_KEY,
+            cifraTone
+        ) ?: ""
+
+        binding.addCifraToneBackImageButton.setOnClickListener {
+            val cifraNameEditText = binding.addCifraToneStringTextView.text
+            val sharedPrefEditor = sharedPref.edit()
+            sharedPrefEditor.putString(
+                Config.SHARED_PREFERENCE_EDIT_CIFRA_MODE_SINGER_NAME_STRING_KEY,
+                cifraNameEditText.toString()
+            )
+            sharedPrefEditor.apply()
+            mainActivityContext.popBackStackFragment()
+        }
+
+        binding.addCifraToneNextImageButton.setOnClickListener {
+            val cifraNameEditText = binding.addCifraToneStringTextView.text
+            if (!cifraNameEditText.isNullOrBlank()) {
+                val sharedPrefEditor = sharedPref.edit()
+                sharedPrefEditor.putString(
+                    Config.SHARED_PREFERENCE_EDIT_CIFRA_MODE_SINGER_NAME_STRING_KEY,
+                    cifraNameEditText.toString()
+                )
+                sharedPrefEditor.apply()
+                mainActivityContext.addToBackStackFragment(AddCifraToneFragment())
+            } else if (cifraNameEditText.isNullOrBlank()) {
+                binding.addCifraToneStringTextView.setHintTextColor(
+                    mainActivityContext.getColorFromAttr(
+                        com.google.android.material.R.attr.colorError
+                    )
+                )
+                mainActivityContext.toastMessage(
+                    R.string.save_cifra_tone_missing_data,
+                    Toast.LENGTH_LONG
+                )
+            }
+        }
+
+        return binding.root
+    }
+
+}
