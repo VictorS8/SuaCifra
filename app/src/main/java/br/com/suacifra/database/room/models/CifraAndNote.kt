3e21eef9a533e94f53f6ff07fb3eb39bdb289055
João Victor <jjoaovvictorneves@gmail.com>
2023-05-06 02:54:26 -0300

    Add cifra and note union

---
+++ app/src/main/java/br/com/suacifra/database/room/models/CifraAndNote.kt
@@ -0,0 +1,27 @@
+package br.com.suacifra.database.room.models
+
+import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.PrimaryKey
+
+@Entity(
+    foreignKeys = [ForeignKey(
+        entity = CifraRoom::class,
+        parentColumns = arrayOf("id"),
+        childColumns = arrayOf("cifraId"),
+        onDelete = ForeignKey.CASCADE,
+        onUpdate = ForeignKey.CASCADE
+    ), ForeignKey(
+        entity = NoteRoom::class,
+        parentColumns = arrayOf("id"),
+        childColumns = arrayOf("noteId"),
+        onDelete = ForeignKey.CASCADE,
+        onUpdate = ForeignKey.CASCADE
+    )]
+)
+data class CifraAndNote(
+    val cifraId: Int,
+    val noteId: Int,
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+)
