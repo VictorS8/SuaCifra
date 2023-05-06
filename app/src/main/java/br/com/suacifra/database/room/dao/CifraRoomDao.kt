aafc1a2a0f5592fda907261ddaadda8225eee2a0
João Victor <jjoaovvictorneves@gmail.com>
2023-05-06 02:53:27 -0300

    Add cifra room model

---
+++ app/src/main/java/br/com/suacifra/database/room/dao/CifraRoomDao.kt
@@ -0,0 +1,31 @@
+package br.com.suacifra.database.room.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Query
+import androidx.room.Transaction
+import androidx.room.Upsert
+import br.com.suacifra.database.room.models.CifraRoom
+import br.com.suacifra.database.room.models.CifraWithSequenceRoom
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface CifraRoomDao {
+
+    @Upsert
+    suspend fun upsertCifra(cifra: CifraRoom)
+
+    @Delete
+    suspend fun deleteCifra(cifra: CifraRoom)
+
+    @Query("SELECT * FROM CifraRoom WHERE id=:cifraId")
+    fun getCifraById(cifraId: Int): CifraRoom
+
+    @Query("SELECT * FROM CifraRoom")
+    fun getAllCifras(): Flow<List<CifraRoom>>
+
+    @Transaction
+    @Query("SELECT * FROM CifraRoom")
+    fun getCifraWithSequence(): List<CifraWithSequenceRoom>
+
+}
---
+++ app/src/main/java/br/com/suacifra/database/room/models/CifraRoom.kt
@@ -0,0 +1,13 @@
+package br.com.suacifra.database.room.models
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class CifraRoom(
+    val name: String,
+    val tone: String,
+    val singerName: String,
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0
+)
