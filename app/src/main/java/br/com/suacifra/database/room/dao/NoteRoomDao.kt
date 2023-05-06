e5fde115bd1f3d491667e18704b4e0e2ade78f48
João Victor <jjoaovvictorneves@gmail.com>
2023-05-06 02:53:40 -0300

    Add note room model

---
+++ app/src/main/java/br/com/suacifra/database/room/dao/NoteRoomDao.kt
@@ -0,0 +1,25 @@
+package br.com.suacifra.database.room.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Query
+import androidx.room.Upsert
+import br.com.suacifra.database.room.models.NoteRoom
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface NoteRoomDao {
+
+    @Upsert
+    suspend fun upsertNote(note: NoteRoom)
+
+    @Delete
+    suspend fun deleteNote(note: NoteRoom)
+
+    @Query("SELECT * FROM NoteRoom WHERE id=:noteId")
+    fun getNoteById(noteId: Int): NoteRoom
+
+    @Query("SELECT * FROM NoteRoom")
+    fun getAllNotes(): Flow<List<NoteRoom>>
+
+}
---
+++ app/src/main/java/br/com/suacifra/database/room/models/NoteRoom.kt
@@ -0,0 +1,12 @@
+package br.com.suacifra.database.room.models
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class NoteRoom(
+    val title: String,
+    val body: String,
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0
+)
